2c01b3eec9b9f7ea23258b7ce6c42df7b601ab51
NestorDanielGomez <nestordanielgomez@gmail.com>
2023-04-10 11:56:14 -0300

    correccion urls

--- next.config.js
+++ next.config.js
@@ -5,9 +5,14 @@
 
 module.exports = {
   env: {
-    backendURL: "http://localhost:4000",
-    frontendURL: "http://localhost:3000",
+    backendURL: "https://data-send-backend.onrender.com",
+    frontendURL: "https://data-send-client.vercel.app",
   },
 };
 
 // module.exports = nextConfig;
+
+// env: {
+//     backendURL: "http://localhost:4000",
+//     frontendURL: "http://localhost:3000",
+//   },
